29d8acfde9d8793c594eba025fa66b63d34c8797
ArtxxDev <artem.bolobov03@gmail.com>
2025-07-13 16:46:43 +0300

    fix commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-  base: "/movies-app"
+  plugins: [react(), tailwindcss()],
+  base: '/'
 })
